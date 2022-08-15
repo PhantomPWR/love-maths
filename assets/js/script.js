9d59126e459ef547a66364745e6a4283bf9d5e03
Jean de Villiers <jeandevilliers@me.com>
2022-08-15 14:33:34 +0000

    Add code for subtraction game

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,6 +37,10 @@ function runGame(gameType) {
 
         displayMultiplyQuestion(num1, num2);
 
+    } else if (gameType === "subtract") {
+
+        displaySubtractQuestion(num1, num2);
+
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -84,6 +88,10 @@ function calculateCorrectAnswer() {
 
         return [operand1 * operand2, "multiply"];
 
+    } else if (operator === "-") {
+
+        return [operand1 - operand2, "subtract"];
+
     } else {
 
         alert(`Unimplemented operator ${operator}`);
@@ -123,8 +131,12 @@ function displayAdditionQuestion(operand1, operand2) {
 }
 
 
-function displaySubtractQuestion() {
-    
+function displaySubtractQuestion(operand1, operand2) {
+
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "-";
+
 }
 
 
